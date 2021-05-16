c6464d704836878e2ba23889f9ac4caf0593d108
rooler <roel.wier@gmail.com>
2021-05-16 16:38:09 -0400

    Look at operator norm instead of fid, negative eigenvalues are not a problem then.

--- demonstrations/classical_shadows_utils.py
+++ demonstrations/classical_shadows_utils.py
@@ -1,17 +1,9 @@
 import numpy as np
 
 
-def trace_fidelity(rho, sigma):
+def operator_2_norm(R):
     """
-    Calculate the trace fidelity F(rho, sigma) = Tr{sqr{sqrt{rho} @ sigma @ sqrt{rho}}}
+    Calculate the operator two norm
     """
-    S_rho = np.linalg.eigvalsh(rho)
-    S_rho[np.isclose(S_rho, 0)] = 0.0
-    S_sigma = np.linalg.eigvalsh(sigma)
-    S_sigma[np.isclose(S_sigma, 0)] = 0.0
+    return np.sqrt(np.trace(R.conjugate().transpose() @ R))
 
-    if np.sum(S_rho < 0) > 0:
-        raise ValueError(f"Negative eigenvalues detected in S_rho: {S_rho}")
-    if np.sum(S_sigma < 0) > 0:
-        raise ValueError(f"Negative eigenvalues detected in S_sigma: {S_sigma}")
-    return np.sum(np.sqrt(np.sqrt(S_rho) * (S_sigma * np.sqrt(S_rho))))
